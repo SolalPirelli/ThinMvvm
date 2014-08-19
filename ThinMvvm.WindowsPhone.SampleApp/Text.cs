ea3f7236401ac16d1e06c7ce5b99e8acd9a22d26
Solal Pirelli <solal.pirelli@gmail.com>
2014-08-19 17:38:34 +0200

    Better localization handling for WP sample project (using a MarkupExtension).

---
+++ ThinMvvm.WindowsPhone.SampleApp/Text.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Solal Pirelli 2014
+// See License.txt file for more details
+
+using System.Windows.Data;
+using ThinMvvm.WindowsPhone.SampleApp.Resources;
+
+namespace ThinMvvm.WindowsPhone.SampleApp
+{
+    public sealed class Text : Binding
+    {
+        private static AppResources _resources = new AppResources();
+
+        public Text( string path )
+            : base( path )
+        {
+            Source = _resources;
+        }
+    }
+}
