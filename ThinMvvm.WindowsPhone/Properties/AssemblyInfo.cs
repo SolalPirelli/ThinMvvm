0a1e943c11437f687bb49568ce1aa09c39248e99
Solal Pirelli <solal.pirelli@gmail.com>
2014-08-21 23:13:59 +0200

    Increased version to 0.9.11.

--- ThinMvvm.WindowsPhone/Properties/AssemblyInfo.cs
+++ ThinMvvm.WindowsPhone/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible( false )]
 [assembly: CLSCompliant( true )]
 
-[assembly: AssemblyVersion( "0.9.10" )]
+[assembly: AssemblyVersion( "0.9.11" )]
--- ThinMvvm/Properties/AssemblyInfo.cs
+++ ThinMvvm/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible( false )]
 [assembly: CLSCompliant( true )]
 
-[assembly: AssemblyVersion( "0.9.10" )]
+[assembly: AssemblyVersion( "0.9.11" )]
 
 [assembly: InternalsVisibleTo( "ThinMvvm.Logging" )]
 [assembly: InternalsVisibleTo( "ThinMvvm.WindowsPhone" )]
