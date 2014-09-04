a18854a9696190173b8e7dccdc30a18541c3bdb5
Solal Pirelli <solal.pirelli@gmail.com>
2014-09-04 17:30:40 +0200

    Forgotten files.

---
+++ ThinMvvm.SampleApp/Models/NewsItem.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Solal Pirelli 2014
+// See License.txt file for more details
+
+using System;
+using System.Runtime.Serialization;
+
+namespace ThinMvvm.SampleApp.Models
+{
+    [DataContract]
+    public sealed class NewsItem : ObservableObject
+    {
+        [DataMember]
+        public string Title { get; private set; }
+
+        [DataMember]
+        public DateTimeOffset Date { get; private set; }
+
+        [DataMember]
+        public string Description { get; private set; }
+
+
+        private bool _isRead;
+        public bool IsRead
+        {
+            get { return _isRead; }
+            set { SetProperty( ref _isRead, value ); }
+        }
+
+
+        public NewsItem( string title, DateTimeOffset date, string description )
+        {
+            Title = title;
+            Date = date;
+            Description = description;
+        }
+    }
+}
---
+++ ThinMvvm.SampleApp/Services/INewsService.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Solal Pirelli 2014
+// See License.txt file for more details
+
+using System.Threading.Tasks;
+using ThinMvvm.SampleApp.Models;
+
+namespace ThinMvvm.SampleApp.Services
+{
+    public interface INewsService
+    {
+        Task<NewsFeed> GetFeedAsync();
+    }
+}
