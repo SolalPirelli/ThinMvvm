54291399b8fbc82d31a12e9e565271e82ca4ed97
Solal Pirelli <solal.pirelli@gmail.com>
2014-09-05 18:53:01 +0200

    Added a forgotten license header.

--- ThinMvvm/ICommandOwnerExtensions.cs
+++ ThinMvvm/ICommandOwnerExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Solal Pirelli 2014
+// See License.txt file for more details
+
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
