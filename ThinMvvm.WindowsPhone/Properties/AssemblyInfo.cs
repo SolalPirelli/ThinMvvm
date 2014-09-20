e8d81f1d8219ea4f2ca950711dddbb73d8bc8ab6
Solal Pirelli <solal.pirelli@gmail.com>
2014-09-20 21:37:56 +0200

    Increased version to 0.9.23.

--- ThinMvvm.WindowsPhone/Properties/AssemblyInfo.cs
+++ ThinMvvm.WindowsPhone/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible( false )]
 [assembly: CLSCompliant( true )]
 
-[assembly: AssemblyVersion( "0.9.22" )]
+[assembly: AssemblyVersion( "0.9.23" )]
